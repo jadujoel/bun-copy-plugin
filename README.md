a7ec9df8f4471dceda22c34cd30b0000c98eae0f
Joel Löf <jlof@evolution.com>
2023-10-06 12:39:22 +0200

    feat: create bun-copy-plugin

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
-# bun-copy-plugin
-Copy files plugin for bun build step
+
+# Bun Copy Plugin
+
+## Setup
+
+```bash
+bun i bun-copy-plugin
+```
+
+## Use
+
+```typescript
+import copy from 'bun-copy-plugin'
+
+Bun.build({
+  entrypoints: ['src/index.ts'],
+  outdir: 'dist',
+  plugins: [
+    copy('static/index.html', 'dist/index.html') // file 'static/index.html' -> 'dist/index.html'
+    copy('static/', 'dist/static') // folder 'static' -> 'dist/static'
+    copy('static/', 'dist') // contents of 'static' -> 'dist/*'
+  ],
+})
+```
---
+++ index.ts
@@ -0,0 +1,49 @@
+/// <reference path="./node_modules/bun-types/types.d.ts" />
+import { BunPlugin } from 'bun';
+import type { Dirent } from 'node:fs';
+import { mkdir, readdir } from 'node:fs/promises';
+import { join } from 'node:path';
+
+export type CopyType = (from: string, to: string) => Promise<void>
+export type CopyPlugin = (from: string, to: string) => BunPlugin
+
+export const copyDirectory: CopyType = async (from, to) => {
+  const files: readonly Dirent[] = await readdir(from, { withFileTypes: true})
+  const promises: readonly Promise<void>[] = files.map(async file => {
+    const infile = join(from, file.name)
+    const outfile = join(to, file.name)
+    if (file.isDirectory()) {
+      await mkdir(outfile, { recursive: true })
+      await copyDirectory(infile, outfile)
+    } else {
+      await copyFile(infile, outfile)
+    }
+  })
+  await Promise.all(promises)
+}
+
+export const copyFile: CopyType = async (from: string, to: string): Promise<void> => {
+  await Bun.write(to, Bun.file(from))
+}
+
+export const copyFilePlugin: CopyPlugin = (from, to) => ({
+  name: 'copyFilePlugin',
+  async setup(): Promise<void> {
+    return copyFile(from, to)
+  },
+} as const)
+
+export const copyDirectoryPlugin: CopyPlugin = (from, to) => ({
+  name: 'copyDirectoryPlugin',
+  async setup(): Promise<void> {
+    await mkdir(to, { recursive: true })
+    return copyDirectory(from, to)
+  }
+} as const)
+
+export const copyPlugin: CopyPlugin = (from, to) =>
+  from.endsWith('/')
+   ? copyDirectoryPlugin(from, to)
+   : copyFilePlugin(from, to)
+
+export default copyPlugin
